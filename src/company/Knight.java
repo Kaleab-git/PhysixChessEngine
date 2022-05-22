ec411f31be4acb9f6f5a03a9dd8e35a13842529f
Physix <61460429+Kaleab-git@users.noreply.github.com>
2022-05-22 21:25:13 +0300

    Modify Knight getMove to include friendly positions (as defended) and enemy king (as attacked) to normal attacked squares

--- src/company/Knight.java
+++ src/company/Knight.java
@@ -25,6 +25,7 @@ public class Knight {
         }
     }
     public static ArrayList getMoves(Board board, boolean inBitboard, boolean isWhite) {
+//        initializing bitboard to enemy kings position so as to consider even enemy kings position as a possible attack square
         long bitboard = 0L;
         ArrayList<Move> moves = new ArrayList<>();
 
@@ -40,13 +41,9 @@ public class Knight {
             if (((knightPositions>>i)&1) == 1){
                 long knightMovesBoard;
 //                avoid capturing your own piece
-                long notMyPieces;
-                if (isWhite) {
-                    notMyPieces = ~board.WHITE_PIECES;
-                }
-                else {
-                    notMyPieces = ~board.BLACK_PIECES;
-                }
+                long enemyKing = isWhite ? board.BK:board.WK;
+                long notMyPieces = isWhite ? ~board.WHITE_PIECES:~board.BLACK_PIECES;
+
 //                We're not shifting in a single direction.
                 if (i > 18) {
                     knightMovesBoard = Board.KNIGHT_SPAN<<(i-18);
@@ -55,14 +52,16 @@ public class Knight {
                     knightMovesBoard = Board.KNIGHT_SPAN>>(18-i);
                 }
 //                Because we're not always shifting to in the same direction, the unreachable files can't be predicted
+//                Also(unrelated) we want to set our attacks bitboard before removing friendly pieces. This way we also consider squares we're defending as sqaures we're attacking.
                 if (i%8 < 4) {
-                    knightMovesBoard &= ~Board.FILE_GH&notMyPieces;
+                    bitboard |= knightMovesBoard&~Board.FILE_GH;
+                    knightMovesBoard &= ~Board.FILE_GH&notMyPieces&~enemyKing;
                 }
                 else {
-                    knightMovesBoard &= ~Board.FILE_AB&notMyPieces;
+                    bitboard |= knightMovesBoard&~Board.FILE_AB;
+                    knightMovesBoard &= ~Board.FILE_AB&notMyPieces&~enemyKing;
                 }
 
-                bitboard |= knightMovesBoard;
                 for (int j = Long.numberOfTrailingZeros(knightMovesBoard);j < 64-Long.numberOfLeadingZeros(knightMovesBoard); j++) {
                     if (((knightMovesBoard>>j)&1) == 1) {
                         moves.add(new Move(i, j));
