4d732fefe6985601e31c3ddfba9f813d30f4f858
Physix <61460429+Kaleab-git@users.noreply.github.com>
2022-05-22 21:23:41 +0300

    Modify bishop getMove to include friendly positions (as defended) and enemy king (as attacked) to normal attacked squares

--- src/company/Bishop.java
+++ src/company/Bishop.java
@@ -8,11 +8,12 @@ public class Bishop {
 
     public static void loadTable(Board board) {
         for (int i=0; i<64; i++) {
-            attacksTo[i] = DAndAntiDMoves(i, board, true);
+            attacksTo[i] = DAndAntiDMoves(i, board, false,true);
         }
     }
 
     public static ArrayList getMoves(Board board, boolean inBitboard, boolean isWhite) {
+//        initializing bitboard to enemy kings position so as to consider even enemy kings position as a possible attack square
         long bitboard = 0L;
         ArrayList<Move> moves = new ArrayList<>();
 
@@ -24,12 +25,17 @@ public class Bishop {
             bishopPosition = board.BB;
         }
         for (int i = Long.numberOfTrailingZeros(bishopPosition);i < 64-Long.numberOfLeadingZeros(bishopPosition); i++) {
-            if (((bishopPosition>>i)&1) == 1){
-                long bishopMovesBoard = DAndAntiDMoves(i, board, isWhite);
-                bitboard |= bishopMovesBoard;
-                for (int j = Long.numberOfTrailingZeros(bishopMovesBoard);j < 64-Long.numberOfLeadingZeros(bishopMovesBoard); j++) {
-                    if (((bishopMovesBoard>>j)&1) == 1) {
-                        moves.add(new Move(i, j));
+            if (inBitboard) {
+                bitboard |= DAndAntiDMoves(i, board, true, isWhite);
+            }
+            else {
+                if (((bishopPosition>>i)&1) == 1){
+                    long enemyKing = isWhite ? board.BK:board.WK;
+                    long bishopMovesBoard = DAndAntiDMoves(i, board, false, isWhite)&~enemyKing;
+                    for (int j = Long.numberOfTrailingZeros(bishopMovesBoard);j < 64-Long.numberOfLeadingZeros(bishopMovesBoard); j++) {
+                        if (((bishopMovesBoard>>j)&1) == 1) {
+                            moves.add(new Move(i, j));
+                        }
                     }
                 }
             }
@@ -41,11 +47,14 @@ public class Bishop {
         return moves;
     }
 
-    static long DAndAntiDMoves(int s, Board board, boolean isWhite) {
+    static long DAndAntiDMoves(int s, Board board, boolean inBitboard, boolean isWhite) {
         long binaryS=1L<<s;
         long possibilitiesDiagonal = ((board.OCCUPIED&Board.DiagonalMasks8[(s / 8) + (s % 8)]) - (2 * binaryS)) ^ Long.reverse(Long.reverse(board.OCCUPIED&Board.DiagonalMasks8[(s / 8) + (s % 8)]) - (2 * Long.reverse(binaryS)));
         long possibilitiesAntiDiagonal = ((board.OCCUPIED&Board.AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]) - (2 * binaryS)) ^ Long.reverse(Long.reverse(board.OCCUPIED&Board.AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]) - (2 * Long.reverse(binaryS)));
         long possibilities = (possibilitiesDiagonal&Board.DiagonalMasks8[(s / 8) + (s % 8)]) | (possibilitiesAntiDiagonal&Board.AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]);
+        if (inBitboard) {
+            return possibilities;
+        }
         if (isWhite) {
             possibilities &= ~board.WHITE_PIECES;
         }
