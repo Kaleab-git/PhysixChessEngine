b319a0d10eb68c03081d2b15d73e0bd24044089f
Physix <61460429+Kaleab-git@users.noreply.github.com>
2022-05-29 13:48:04 +0300

    implement castling as a king move (Board already has support for castling move)

--- src/company/King.java
+++ src/company/King.java
@@ -8,15 +8,15 @@ public class King {
         long bitboard = 0L;
         ArrayList moves = new ArrayList();
 
-        long kingPositions;
+        long kingPosition;
         if (isWhite) {
-            kingPositions = board.WK;
+            kingPosition = board.WK;
         }
         else {
-            kingPositions = board.BK;
+            kingPosition = board.BK;
         }
-        for (int i = Long.numberOfTrailingZeros(kingPositions);i < 64-Long.numberOfLeadingZeros(kingPositions); i++) {
-            if (((kingPositions>>i)&1) == 1){
+        for (int i = Long.numberOfTrailingZeros(kingPosition);i < 64-Long.numberOfLeadingZeros(kingPosition); i++) {
+            if (((kingPosition>>i)&1) == 1){
                 long kingMovesBoard;
 //                avoid capturing your own piece
                 long notMyPieces;
@@ -48,8 +48,8 @@ public class King {
 //                1. Inorder to avoid infinitely calling on eachother
 //                2. Just because enemy king can't move to a square because original piece is attacking that square, it does not mean that enemy king is not attacking that square
 //                   Original king can't move to an attacked cell thinking that it has got its own piece to defend it
+                long enemyAttacks = 0L;
                 if (!reconnaissanceCall) {
-                    long enemyAttacks = 0L;
                     enemyAttacks |= (long) Pawn.getMoves(board, true, !isWhite).get(0);
                     enemyAttacks |= (long) Rook.getMoves(board, true, !isWhite).get(0);
                     enemyAttacks |= (long) Bishop.getMoves(board, true, !isWhite).get(0);
@@ -57,6 +57,27 @@ public class King {
                     enemyAttacks |= (long) Queen.getMoves(board, true, !isWhite).get(0);
                     enemyAttacks |= (long) King.getMoves(board, true, !isWhite, true).get(0);
                     kingMovesBoard &= (~enemyAttacks);
+                }
+//              Casteling
+                boolean kingSide = isWhite ? board.WKC:board.BKC;
+                boolean queenSide = isWhite ? board.WQC:board.WKC;
+//                Squares inbetween King and rook depending on color and side
+                long kingSideSquares = isWhite ? 6917529027641081856L:96;
+                long queenSideSqueares = isWhite ? 1008806316530991104L:14;
+/*                    Make sure that: the current player has Kingside castling right, the king's position and none of the squares in between are under attack,
+                      that the squares in between are empty */
+
+                if (kingSide && ((kingPosition | kingSideSquares) & enemyAttacks) == 0 && (kingSideSquares & board.EMPTY) == kingSideSquares) {
+                    String moveNotation = isWhite ? "e1,g1":"e8,g8";
+                    Move move = new Move(moveNotation);
+                    move.type = "Castling";
+                    moves.add(move);
+                }
+                if (queenSide && ((kingPosition | queenSideSqueares) & enemyAttacks) == 0 && (queenSideSqueares & board.EMPTY) == queenSideSqueares) {
+                    String moveNotation = isWhite ? "e1,c1":"e8,c8";
+                    Move move = new Move(moveNotation);
+                    move.type = "Castling";
+                    moves.add(move);
                 }
                 for (int j = Long.numberOfTrailingZeros(kingMovesBoard);j < 64-Long.numberOfLeadingZeros(kingMovesBoard); j++) {
                     if (((kingMovesBoard>>j)&1) == 1) {
